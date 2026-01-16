20bcccb86d06c36c66300c386f7073d688ec28c2
Hegodi <diegonher@gmail.com>
2026-01-16 07:17:19 +0100

    Add AppMain and README

---
+++ AppMain.h
@@ -0,0 +1,55 @@
+#include <olcPixelGameEngine.h>
+
+#include <UI/UIBase.h>
+
+class AppMain : public olc::PixelGameEngine
+{
+public:
+	AppMain(const char* appName)
+	{
+		sAppName = appName;
+	}
+
+	virtual void Update(float dt) = 0;
+	void SetClearColor(olc::Pixel color) { mColorClear = color; }
+
+	template<typename T, typename... Args>
+	std::shared_ptr<T> CreateUiElement(Args&&... args)
+	{
+		static_assert(std::is_base_of_v<UIBase, T>);
+		std::shared_ptr<T> uiElement = std::make_shared<T>(std::forward<Args>(args)...);
+		mUiElements.push_back(std::shared_ptr<UIBase>(uiElement));
+		return uiElement;
+	}
+
+	void ClearUiElements()
+	{
+		mUiElements.clear();
+	}
+
+private:
+	bool OnUserUpdate(float fElapsedTime) final
+	{
+		Clear(mColorClear);
+
+		Update(fElapsedTime);
+		UpdateUIElements();
+
+		return true;
+	}
+
+	void UpdateUIElements()
+	{
+		for (auto& ui : mUiElements)
+		{
+			ui->Update(this);
+		}
+	}
+
+
+private:
+	olc::Pixel mColorClear = olc::BLACK;
+public:
+	std::vector<std::shared_ptr<UIBase>> mUiElements;
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This repo contains helpers and modules to be used with olc::PixelGameEngine.
 
+The basic usage is to make your main class inherit from AppMain instead of olc::PixelGameEngine. 
+
